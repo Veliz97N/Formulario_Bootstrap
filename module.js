9a50662eda1c526e4c68369d5148d858530016cb
Nicolas <nicolas.veliz@usach.cl>
2021-08-22 12:20:53 -0400

    Se agrega boton cancel, funcion para State y funcion para metodos de pago

--- module.js
+++ module.js
@@ -15,9 +15,46 @@ const state = document.getElementById("inputState");
 const invalidState=document.getElementById("invalid-State");
 const postalCode = document.getElementById("inputPostalCode");
 const invalidPostcalCode=document.getElementById("invalid-PostalCode");
-
 const message = document.getElementById("inputMessage");
 const invalidMessage=document.getElementById("invalid-Message");
+const paymentMethod= document.getElementsById("paymentMethod"); //ACA VA PAYMENT METHOD
+const cancel=document.getElementById("Cancel");
+
+function Restart(){
+    card.style.backgroundColor = "white";
+    invalidCard.style.visibility="hidden"; //Fin Card
+    cvc.style.backgroundColor = "white";
+    invalidCvc.style.visibility="hidden"; // Fin CVC
+    amount.style.backgroundColor = "white";
+    invalidAmount.style.visibility="hidden";  //Fin Amount
+    firstName.style.backgroundColor = "white";
+    invalidFirstName.style.visibility="hidden"; //Fin First Name 
+    lastName.style.backgroundColor = "white";
+    invalidLastName.style.visibility="hidden"; //Fin Last Name
+    city.style.backgroundColor = "white";
+    invalidCity.style.visibility="hidden"; //Fin City
+    state.style.backgroundColor = "white";
+    invalidState.style.visibility="hidden"; //Fin State
+    postalCode.style.backgroundColor = "white";
+    invalidPostcalCode.style.visibility="hidden"; //Fin Postal Code
+    message.style.backgroundColor = "white";
+    invalidMessage.style.visibility="hidden"; //Fin Message
+};
+function Verificar_RadioButton(){
+    let verificar=null;
+    const metodos_pago=document.getElementsByName("radio_payment");
+    for(let x=0;x<metodos_pago.length;x++){
+        if(metodos_pago[x].checked){
+            verificar=true;
+            break;
+        }
+        else{
+            verificar=false;
+        }
+    }
+    return verificar;
+};
+
 
 formulario.addEventListener("submit", (e) => {
     if (card.value == "" || card.value == null || card.value.length < 16) {
@@ -25,45 +62,84 @@ formulario.addEventListener("submit", (e) => {
         card.style.backgroundColor = "red";
         invalidCard.style.visibility="visible";    
     }
+    else{
+        card.style.backgroundColor = "white";
+        invalidCard.style.visibility="hidden"; 
+    }
     if (cvc.value == "" || cvc.value == null || cvc.value.length < 3) {
         e.preventDefault();
         cvc.style.backgroundColor = "red";
         invalidCvc.style.visibility="visible";   
-
     }
+    else{
+        cvc.style.backgroundColor = "white";
+        invalidCvc.style.visibility="hidden"; 
+    } 
     if (amount.value == "" || amount.value == null) {
         e.preventDefault();
         amount.style.backgroundColor = "red";
         invalidAmount.style.visibility="visible";
+    }
+    else{
+        amount.style.backgroundColor = "white";
+        invalidAmount.style.visibility="hidden"; 
     }
     if (firstName.value == "" || firstName.value == null) {
         e.preventDefault();
         firstName.style.backgroundColor = "red";
         invalidFirstName.style.visibility="visible";
     }
+    else{
+        firstName.style.backgroundColor = "white";
+        invalidFirstName.style.visibility="hidden"; 
+    }
     if (lastName.value == "" || lastName.value == null) {
         e.preventDefault();
         lastName.style.backgroundColor = "red";
         invalidLastName.style.visibility="visible";
+    }
+    else{
+        lastName.style.backgroundColor = "white";
+        invalidLastName.style.visibility="hidden"; 
     }
     if (city.value == "" || city.value == null) {
         e.preventDefault();
         city.style.backgroundColor = "red";
         invalidCity.style.visibility="visible";
     }
-    if(state.value=="" || state.value==null){
+    else{
+        city.style.backgroundColor = "white";
+        invalidCity.style.visibility="hidden"; 
+    }
+    if(state.options[0].selected){
         e.preventDefault();
         state.style.backgroundColor="red";
         invalidState.style.visibility="visible";
     }  
+                else{
+        state.style.backgroundColor = "white";
+        invalidState.style.visibility="hidden"; 
+    }
     if(postalCode.value=="" ||postalCode.value==null){
         e.preventDefault();
         postalCode.style.backgroundColor="red";
         invalidPostcalCode.style.visibility="visible";
     }
+    else{
+        postalCode.style.backgroundColor = "white";
+        invalidPostcalCode.style.visibility="hidden"; 
+    }
     if(message.value==""||message.value==null){
         e.preventDefault();
         message.style.backgroundColor="red";
         invalidMessage.style.visibility="visible";
+    }
+    else{
+        message.style.backgroundColor = "white";
+        invalidMessage.style.visibility="hidden"; 
     }    
-})
+});
+
+cancel.addEventListener("click",()=>{
+    Restart();
+});
